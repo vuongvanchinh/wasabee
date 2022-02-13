beecc6b0f228d6fc899bcdd51b0d54ebd85b5d11
Sotatek-ChinhVuong <chinh.vuong@sotatek.com>
2022-02-13 23:34:21 +0700

    page 1

--- js/slider.js
+++ js/slider.js
@@ -26,30 +26,26 @@ $(document).ready(() => {
         // nextArrow: '<button class="slider-arrow next-arrow">Next</button>'
     });
 
-        $(".banner-slider").slick({
-        slidesToShow:1,
-        slidesToScroll: 1,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                arrows: false,
-                // centerMode: true,
-                // centerPadding: '40px',
-                slidesToShow: 3
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                arrows: false,
-                // centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-              }
+      $(".landing-slider").slick({
+      slidesToShow:1,
+      slidesToScroll: 1,
+      responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              slidesToShow: 3
             }
-          ]
-        // prevArrow: '<button class="slider-arrow prev-arrow">Pre</button>',
-        // nextArrow: '<button class="slider-arrow next-arrow">Next</button>'
-    });
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              // centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+  });
 })
