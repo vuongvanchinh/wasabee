55626b1ee98d9eb2e61032b57c1d165c8591a90d
Sotatek-ChinhVuong <chinh.vuong@sotatek.com>
2022-02-15 09:15:43 +0700

    update

--- js/slider.js
+++ js/slider.js
@@ -48,4 +48,43 @@ $(document).ready(() => {
           }
         ]
   });
+
+  $(".slider-style").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          // arrows: false,
+          // centerMode: true,
+          // centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+        {
+          breakpoint: 650,
+          settings: {
+            arrows: false,
+            // centerMode: true,
+            // centerPadding: '40px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            // centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+});
+$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+  e.target
+  e.relatedTarget
+  $('.slider-style').slick('setPosition');
+});
 })
